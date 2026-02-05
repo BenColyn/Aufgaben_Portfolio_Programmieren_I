ef2c683f2f55da57e9501259cb7cb688d3544872
BenColyn <bencolyn4@gmail.com>
2026-02-05 10:16:42 +0100

    Auto Vrumm Vrumm update Engine.cpp und Engine.h

---
+++ Aufgabe6/Engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by benco on 05.02.2026.
+//
+
+#include "Engine.h"
--- Aufgabe6/Engine.h
+++ Aufgabe6/Engine.h
@@ -1,32 +1,40 @@
-#ifndef COMPONENT_H
-#define COMPONENT_H
+//
+// Created by benco on 05.02.2026.
+//
+
+#ifndef TESTPROJEKT_ENGINE_H
+#define TESTPROJEKT_ENGINE_H
 #include <string>
 
-
-
-namespace car::component {
-    // Engine
+namespace car:: component {
     class Engine {
     public:
         virtual ~Engine() = default;
+
         virtual int getHP() const = 0;
+
         virtual std::string getName() const = 0;
+
         void start() { running = true; }
         void stop() { running = false; }
         bool isRunning() const { return running; }
+
     protected:
         bool running = false;
     };
+
     class V8Engine : public Engine {
     public:
         int getHP() const override { return 580; }
         std::string getName() const override { return "V8-Aggregat"; }
     };
+
     class DieselEngine : public Engine {
     public:
         int getHP() const override { return 320; }
         std::string getName() const override { return "Reihen-6-Zylinder Diesel"; }
     };
+};
 
-}
-#endif
+
+#endif //TESTPROJEKT_ENGINE_H
